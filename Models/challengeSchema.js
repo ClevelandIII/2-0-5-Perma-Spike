70e0ff665e532dab673215798ac19735e1f035e0
Sean Zimbelman <Szimbe295@west-mec.local>
2021-11-22 16:24:49 -0700

    schema stuff

--- Models/challengeSchema.js
+++ Models/challengeSchema.js
@@ -1,87 +1,111 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const challengeSchema = new mongoose.Schema (
-    {
-        // createdBy: {
-        //     type: mongoose.Types.ObjectId,
-        //     ref: "User",
-        //     required: [true, "Please provide user"],
-        //   },
-        Name: {
-            type: String,
-            required: [true, 'Must enter a name'],
-            maxlength: 40
-        },
-        Map: {
-            type: String,
-            required: [true, 'Must enter a map'],
-        },
-        Difficulty: {
-            type: String,
-            default: "medium",
-            enum: ["easy", "medium", "hard"]
-        },
-        Lives: {
-            type: Number,
-            default: 150
-        },
-        Cash: {
-            type: Number,
-            default: 650
-        },
-        Extras: {
-            type: Object,
-            powers: {
-                type: Boolean,
-                default: true
-            },
-            selling: {
-                type: Boolean,
-                default: true
-            },
-            allCamo: {
-                type: Boolean,
-                default: false
-            },
-            doubleCashDisabled: {
-                type: Boolean,
-                default: false
-            },
-            monkeyKnowledge: {
-                type: Boolean,
-                default: true
-            },
-            continues: {
-                type: Boolean,
-                default: true
-            },
-            allRegrow: {
-                type: Boolean,
-                default: false
-            },
-            bloonSpeed: {
-                type: Number,
-                default: 100
-            },
-            moabSpeed: {
-                type: Number,
-                default: 100
-            },
-            ceramicHealth: {
-                type: Number,
-                default: 100
-            },
-            moabHealth: {
-                type: Number,
-                default: 100
-            },
-            regrowRate: {
-                type: Number,
-                default: 100
-            },
-
-        },
+const challengeSchema = new mongoose.Schema({
+  // createdBy: {
+  //     type: mongoose.Types.ObjectId,
+  //     ref: "User",
+  //     required: [true, "Please provide user"],
+  //   },
+  Name: {
+    type: String,
+    required: [true, "Must enter a name"],
+    maxlength: 40,
+  },
+  Map: {
+    type: String,
+    required: [true, "Must enter a map"],
+  },
+  Difficulty: {
+    type: String,
+    default: "medium",
+    enum: ["easy", "medium", "hard"],
+  },
+  Lives: {
+    type: Number,
+    default: 150,
+  },
+  Cash: {
+    type: Number,
+    default: 650,
+  },
+  Monkeys: {
+    type: Object,
+    max: {
+        type: Number,
+        default: 20
+    },
+    banned: {
+        type: Array,
+        default: []
     }
-)
+},
+  Options: {
+    type: Object,
+    powers: {
+      type: Boolean,
+      default: true,
+    },
+    selling: {
+      type: Boolean,
+      default: true,
+    },
+    allCamo: {
+      type: Boolean,
+      default: false,
+    },
+    doubleCashDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    monkeyKnowledge: {
+      type: Boolean,
+      default: true,
+    },
+    continues: {
+      type: Boolean,
+      default: true,
+    },
+    allRegrow: {
+      type: Boolean,
+      default: false,
+    },
+    bloonSpeed: {
+      type: Number,
+      default: 100,
+    },
+    moabSpeed: {
+      type: Number,
+      default: 100,
+    },
+    ceramicHealth: {
+      type: Number,
+      default: 100,
+    },
+    moabHealth: {
+      type: Number,
+      default: 100,
+    },
+    regrowRate: {
+      type: Number,
+      default: 100,
+    },
+    chimps: {
+      type: Boolean,
+      default: false,
+    },
+    alternateBloonsRounds: {
+      type: Boolean,
+      default: false,
+    },
+    halfCash: {
+      type: Boolean,
+      default: false,
+    },
+    apopalypse: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
 
-module.exports = mongoose.model("challenge", challengeSchema)
+module.exports = mongoose.model("challenge", challengeSchema);
--- Models/odysseySchema.js
+++ Models/odysseySchema.js
@@ -1,80 +1,451 @@
 const mongoose = require('mongoose')
 
-const challengeSchema = new mongoose.Schema (
+const odysseySchema = new mongoose.Schema (
     {
         Name: {
             type: String,
             required: [true, 'Must enter a name'],
             maxlength: 40
         },
-        Maps: {
-            type: Array,
-            required: [true, 'Must enter map'],
-        },
-        Difficulty: {
-            type: String,
-            default: "medium",
-            match: [["easy", "medium", "hard"], "Must provide a valid difficulty"]
-        },
         Lives: {
             type: Number,
-            default: 150
+            default: 50
         },
-        Cash: {
-            type: Number,
-            default: 650
-        },
-        Extras: {
+        Options: {
             type: Object,
-            powers: {
-                type: Boolean,
-                default: true
-            },
-            selling: {
-                type: Boolean,
-                default: true
-            },
-            allCamo: {
+            extremeMode: {
                 type: Boolean,
                 default: false
-            },
-            doubleCashDisabled: {
-                type: Boolean,
-                default: false
-            },
-            monkeyKnowledge: {
-                type: Boolean,
-                default: true
-            },
-            continues: {
-                type: Boolean,
-                default: true
-            },
-            allRegrow: {
-                type: Boolean,
-                default: false
-            },
-            bloonSpeed: {
+            }
+        },
+        Monkeys: {
+            type: Object,
+            max: {
                 type: Number,
-                default: 100
+                default: 20
             },
-            moabSpeed: {
-                type: Number,
-                default: 100
-            },
-            ceramicHealth: {
-                type: Number,
-                default: 100
-            },
-            moabHealth: {
-                type: Number,
-                default: 100
-            },
-            regrowRate: {
-                type: Number,
-                default: 100
-            },
+            banned: {
+                type: Array,
+                default: []
+            }
+        },
+        Games: {
+            type: Object,
+            required: [true, 'Must enter Games'],
 
+            gameOne: {
+                required: true,
+                Cash: {
+                    type: Number,
+                    default: 650
+                },
+                Map: {
+                    type: String,
+                    required: [true, 'Must enter a map'],
+                },
+                Difficulty: {
+                    type: String,
+                    default: "medium",
+                    enum: ["easy", "medium", "hard"]
+                },
+                options: {
+                    type: Object,
+                    powers: {
+                        type: Boolean,
+                        default: true
+                    },
+                    selling: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allCamo: {
+                        type: Boolean,
+                        default: false
+                    },
+                    doubleCashDisabled: {
+                        type: Boolean,
+                        default: false
+                    },
+                    monkeyKnowledge: {
+                        type: Boolean,
+                        default: true
+                    },
+                    continues: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allRegrow: {
+                        type: Boolean,
+                        default: false
+                    },
+                    bloonSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    ceramicHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    regrowRate: {
+                        type: Number,
+                        default: 100
+                    },
+                    chimps: {
+                        type: Boolean,
+                        default: false
+                    },
+                    alternateBloonsRounds: {
+                        type: Boolean,
+                        default: false
+                    },
+                    halfCash: {
+                        type: Boolean,
+                        default: false
+                    },
+                    apopalypse: {
+                        type: Boolean,
+                        default: false
+                    },
+                },
+            },
+            gameTwo: {
+                required: true,
+                Cash: {
+                    type: Number,
+                    default: 650
+                },
+                Map: {
+                    type: String,
+                    required: [true, 'Must enter a map'],
+                },
+                Difficulty: {
+                    type: String,
+                    default: "medium",
+                    enum: ["easy", "medium", "hard"]
+                },
+                options: {
+                    type: Object,
+                    powers: {
+                        type: Boolean,
+                        default: true
+                    },
+                    selling: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allCamo: {
+                        type: Boolean,
+                        default: false
+                    },
+                    doubleCashDisabled: {
+                        type: Boolean,
+                        default: false
+                    },
+                    monkeyKnowledge: {
+                        type: Boolean,
+                        default: true
+                    },
+                    continues: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allRegrow: {
+                        type: Boolean,
+                        default: false
+                    },
+                    bloonSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    ceramicHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    regrowRate: {
+                        type: Number,
+                        default: 100
+                    },
+                    chimps: {
+                        type: Boolean,
+                        default: false
+                    },
+                    alternateBloonsRounds: {
+                        type: Boolean,
+                        default: false
+                    },
+                    halfCash: {
+                        type: Boolean,
+                        default: false
+                    },
+                    apopalypse: {
+                        type: Boolean,
+                        default: false
+                    },
+                },
+            },
+            gameThree: {
+                required: true,
+                Cash: {
+                    type: Number,
+                    default: 650
+                },
+                Map: {
+                    type: String,
+                    required: [true, 'Must enter a map'],
+                },
+                Difficulty: {
+                    type: String,
+                    default: "medium",
+                    enum: ["easy", "medium", "hard"]
+                },
+                options: {
+                    type: Object,
+                    powers: {
+                        type: Boolean,
+                        default: true
+                    },
+                    selling: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allCamo: {
+                        type: Boolean,
+                        default: false
+                    },
+                    doubleCashDisabled: {
+                        type: Boolean,
+                        default: false
+                    },
+                    monkeyKnowledge: {
+                        type: Boolean,
+                        default: true
+                    },
+                    continues: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allRegrow: {
+                        type: Boolean,
+                        default: false
+                    },
+                    bloonSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    ceramicHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    regrowRate: {
+                        type: Number,
+                        default: 100
+                    },
+                    chimps: {
+                        type: Boolean,
+                        default: false
+                    },
+                    alternateBloonsRounds: {
+                        type: Boolean,
+                        default: false
+                    },
+                    halfCash: {
+                        type: Boolean,
+                        default: false
+                    },
+                    apopalypse: {
+                        type: Boolean,
+                        default: false
+                    },
+                },
+            },
+            gameFour: {
+                Cash: {
+                    type: Number,
+                    default: 650
+                },
+                Map: {
+                    type: String,
+                    required: [true, 'Must enter a map'],
+                },
+                Difficulty: {
+                    type: String,
+                    default: "medium",
+                    enum: ["easy", "medium", "hard"]
+                },
+                options: {
+                    type: Object,
+                    powers: {
+                        type: Boolean,
+                        default: true
+                    },
+                    selling: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allCamo: {
+                        type: Boolean,
+                        default: false
+                    },
+                    doubleCashDisabled: {
+                        type: Boolean,
+                        default: false
+                    },
+                    monkeyKnowledge: {
+                        type: Boolean,
+                        default: true
+                    },
+                    continues: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allRegrow: {
+                        type: Boolean,
+                        default: false
+                    },
+                    bloonSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    ceramicHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    regrowRate: {
+                        type: Number,
+                        default: 100
+                    },
+                    chimps: {
+                        type: Boolean,
+                        default: false
+                    },
+                    alternateBloonsRounds: {
+                        type: Boolean,
+                        default: false
+                    },
+                    halfCash: {
+                        type: Boolean,
+                        default: false
+                    },
+                    apopalypse: {
+                        type: Boolean,
+                        default: false
+                    },
+                },
+            },
+            gameFive: {
+                Cash: {
+                    type: Number,
+                    default: 650
+                },
+                Map: {
+                    type: String,
+                    required: [true, 'Must enter a map'],
+                },
+                Difficulty: {
+                    type: String,
+                    default: "medium",
+                    enum: ["easy", "medium", "hard"]
+                },
+                options: {
+                    type: Object,
+                    powers: {
+                        type: Boolean,
+                        default: true
+                    },
+                    selling: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allCamo: {
+                        type: Boolean,
+                        default: false
+                    },
+                    doubleCashDisabled: {
+                        type: Boolean,
+                        default: false
+                    },
+                    monkeyKnowledge: {
+                        type: Boolean,
+                        default: true
+                    },
+                    continues: {
+                        type: Boolean,
+                        default: true
+                    },
+                    allRegrow: {
+                        type: Boolean,
+                        default: false
+                    },
+                    bloonSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabSpeed: {
+                        type: Number,
+                        default: 100
+                    },
+                    ceramicHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    moabHealth: {
+                        type: Number,
+                        default: 100
+                    },
+                    regrowRate: {
+                        type: Number,
+                        default: 100
+                    },
+                    chimps: {
+                        type: Boolean,
+                        default: false
+                    },
+                    alternateBloonsRounds: {
+                        type: Boolean,
+                        default: false
+                    },
+                    halfCash: {
+                        type: Boolean,
+                        default: false
+                    },
+                    apopalypse: {
+                        type: Boolean,
+                        default: false
+                    },
+                },
+            },
         },
     }
 )
