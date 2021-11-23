74524cf4c0bcb1b6d02242ecc6d61cca5663a4f8
Cleveland Williams <Cwilli290@west-mec.local>
2021-11-22 18:29:04 -0700

    delete

--- Controllers/challenge.js
+++ Controllers/challenge.js
@@ -1,27 +1,34 @@
 const { StatusCodes } = require("http-status-codes");
 const Challenge = require("../Models/challengeSchema");
+const NotFoundError = require("../Error/notFound");
 
 const getChallenge = async (req, res) => {
-  const { userID } = req.user.userID;
+  const { userID } = req.user;
   const { id: challengeID } = req.params;
 
-  const challenge = await Job.findOne({ createdBy: userID, _id: challengeID });
+  const challenge = await Challenge.findOne({
+    createdBy: userID,
+    _id: challengeID,
+  });
 
   if (!challenge) {
-    throw new NotFoundError(`${challengeID} not found`);
+    throw new NotFoundError(`${challengeID} not found!`);
   }
 
   res.status(StatusCodes.OK).json({ challenge });
 };
-const getAllChallenge = async (req, res) => {
-  const challenge = await Challenge.find({ createdBy: req.userID }).sort("created at");
+
+const getAllChallenges = async (req, res) => {
+  const challenge = await Challenge.find({ createdBy: req.user.userID }).sort(
+    "created at"
+  );
   res.status(StatusCodes.OK).json({ challenge, length: challenge.length });
 };
 
 const createChallenge = async (req, res) => {
   req.body.createdBy = req.user.userID;
   const challenge = await Challenge.create(req.body);
-  console.log(challenge)
+  console.log(challenge);
   res.status(StatusCodes.CREATED).json({ challenge });
 };
 
@@ -30,12 +37,22 @@ const updateChallenge = async (req, res) => {
 };
 
 const deleteChallenge = async (req, res) => {
-  res.send("Delete Challenge");
+  const {
+    params: { id: challengeID },
+    user: { userID },
+  } = req;
+
+  const challenge = await Challenge.findOneAndRemove({
+    _id: challengeID,
+    createdBy: userID,
+  });
+
+  res.status(StatusCodes.OK).json({ challenge });
 };
 
 module.exports = {
   getChallenge,
-  getAllChallenge,
+  getAllChallenges,
   createChallenge,
   updateChallenge,
   deleteChallenge,
