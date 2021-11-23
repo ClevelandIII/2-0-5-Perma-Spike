1a62ddc57691482f2a9b258dbe141758f127d60c
Sean Zimbelman <Szimbe295@west-mec.local>
2021-11-22 18:34:28 -0700

    chellenge stuff

--- Controllers/challenge.js
+++ Controllers/challenge.js
@@ -14,19 +14,51 @@ const getChallenge = async (req, res) => {
   res.status(StatusCodes.OK).json({ challenge });
 };
 const getAllChallenge = async (req, res) => {
-  const challenge = await Challenge.find({ createdBy: req.userID }).sort("created at");
+  const challenge = await Challenge.find({ createdBy: req.userID }).sort(
+    "created at"
+  );
   res.status(StatusCodes.OK).json({ challenge, length: challenge.length });
 };
 
 const createChallenge = async (req, res) => {
   req.body.createdBy = req.user.userID;
   const challenge = await Challenge.create(req.body);
-  console.log(challenge)
+  console.log(challenge);
   res.status(StatusCodes.CREATED).json({ challenge });
 };
 
 const updateChallenge = async (req, res) => {
-  res.send("Update Challenge");
+  const {
+    body: {
+      Name: name,
+      Map: map,
+      Difficulty: difficulty,
+      Lives: lives,
+      Cash: cash,
+      Monkeys: monkeys,
+      Options: options,
+    },
+    user: { userID },
+    params: { id: challengeID },
+  } = req;
+
+  // if (!name || !map || !difficulty || !lives || !cash || !monkeys || !options) {
+  //   throw new BadRequestError(
+  //     "Please provide a name, and map, difficulty, and lives, and cash, and monkeys, and options"
+  //   );
+  // }
+
+  const challenge = await Challenge.findByIdAndUpdate(
+    { _id: challengeID, createdBy: userID },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!challenge) {
+    throw new BadRequestError(`no new job with ${jobID}`);
+  }
+
+  res.status(StatusCodes.OK).json({ challenge });
 };
 
 const deleteChallenge = async (req, res) => {
